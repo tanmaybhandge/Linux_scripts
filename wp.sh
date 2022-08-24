2d6dd32ab6d7fb2cab53ebf76b51ea9b299060a0
tanmaybhandge <111745693+tanmaybhandge@users.noreply.github.com>
2022-08-24 12:47:30 +0530

    Update wp.sh

--- wp.sh
+++ wp.sh
@@ -2,12 +2,13 @@
 
 # DB, Wordpress 초기비번 정의 /root/INIT_PASSWORD
 INIT_ID="root"
-INIT_PASSWORD="ODFiOTBhOG"
-dbhost="10.38.78.56"
-MYIP="10.38.78.51"
+INIT_PASSWORD="NzA1YjY0OT"
+dbhost="10.38.78.57"
+MYIP="10.38.78.52"
 
 curl -s --data "dbname=wordpress&uname=$INIT_ID&pwd=$INIT_PASSWORD&dbhost=$dbhost&prefix=wp_&submit=Submit" \
 http://$MYIP/wp-admin/setup-config.php?step=2
+
 curl -s --data "weblog_title=CloudZ+Wordpress+QuickStart&user_name=$INIT_ID&admin_password=$INIT_PASSWORD& \
 admin_password2=$INIT_PASSWORD&admin_email=admin%40example.com&blog_public=1&Submit=Install+WordPress" \
 http://$MYIP/wp-admin/install.php?step=2
