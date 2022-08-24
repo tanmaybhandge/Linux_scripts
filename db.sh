646793568765eef096691e1d52bd289b551cba3b
tanmaybhandge <111745693+tanmaybhandge@users.noreply.github.com>
2022-08-24 15:11:47 +0530

    Update db.sh

--- db.sh
+++ db.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+sudo yum install -y mariadb-server
+
+#Enable and start db
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
 # DB, Wordpress 초기비번 정의 /root/INIT_PASSWORD
 INIT_ID="root"
 INIT_PASSWORD=$(date +%s | sha256sum | base64 | head -c 10)
 echo $INIT_PASSWORD > /root/INIT_PASSWORD
+
+yum update -y
+yum install -y httpd php mariadb-server php-mysqlnd expect
+
+chown -R apache:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+
+systemctl start httpd
+systemctl enable httpd
+systemctl start mariadb
+systemctl enable mariadb
 
 # DB 비번 초기화 ============================
 /usr/bin/mysqladmin -u root password "$INIT_PASSWORD"
@@ -37,3 +56,5 @@ GRANT ALL PRIVILEGES ON *.* TO '$INIT_ID'@'localhost' IDENTIFIED BY '$INIT_PASSW
 GRANT ALL PRIVILEGES ON *.* TO '$INIT_ID'@'%'  IDENTIFIED BY '$INIT_PASSWORD' WITH GRANT OPTION;
 FLUSH PRIVILEGES;\
 "
+
+
